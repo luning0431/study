1595684051d24894ffd41a49a3cda6f3982bed8e
bamboo <luning0431@sina.com>
2026-05-19 22:45:33 +0800

    initial commit : add two core programs

---
+++ exec1.py
@@ -0,0 +1,20 @@
+import os
+import datetime
+import exec2
+def  main():
+
+    filename="test1"
+    print(f"begin")
+    if os.path.exists(filename):
+        os.remove(filename)
+        print(f"delete'{filename}'")
+
+    currenttime=datetime.datetime.now().strftime("%Y-%m-%D:%h:%m:%S")
+    with open(filename,"w",encoding="utf-8") as f:
+        myanswer=exec2.answer()    
+        f.write(myanswer)
+        f.write(currenttime)
+    print(f"create '{filename}'")
+
+if __name__=="__main__": 
+    main()
---
+++ exec2.py
@@ -0,0 +1,6 @@
+import os
+import time
+
+def answer():
+    print(f"answering")    
+    return f"anser1"
